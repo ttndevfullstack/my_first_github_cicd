142089059f8a37f60a949a2efdaaf1dc5f61c9cb
iam-truongtrungnghia <nghiatt@qasoftsolution.com>
2024-09-07 10:42:10 +0700

    feat: add say beautifull

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def hello():
     print("Hello world!!!")
+
+def beautifull():
+    print("You are beautifull!!!")
     
 def goobye():
     print("Goodbye!!!")
 
 print(hello())
+print(beautifull())
 print(goobye())
